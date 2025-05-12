418e8979ee5d1fe6af11b67d9e14c30bda06658b
TheCoderKamal <kamalsagpariya07@gmail.com>
2025-05-13 02:43:20 +0530

    commit

--- P017_InsertionSort.java
+++ P017_InsertionSort.java
@@ -2,13 +2,13 @@ public class P017_InsertionSort {
     public static void main(String[] args) {
         int[] arr = {5, 2, 9, 1, 5, 6};
         
-        for(int i = 0; i < arr.length; i++){
-            for(int j = i; j > 0 ; j--){
-                while(arr[j] < arr[j - 1]){
-                    int temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                }
+        for(int i = 1; i < arr.length; i++){
+            int j = i;
+            while(j > 0 && arr[j] < arr[j - 1]){
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
             }
         }
 
